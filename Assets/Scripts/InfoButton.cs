6f4524fa4cb5a466e0563d276bbdb363f066b4d1
Joshua Byron <joshuabyron61@yahoo.com>
2021-01-27 20:00:48 -0500

    Added menu scene and added Git plugin

---
+++ Assets/Scripts/InfoButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoButton : MonoBehaviour
+{
+    public void onClick()
+    {
+        Debug.Log("Clicked Info Button");
+    }
+}
